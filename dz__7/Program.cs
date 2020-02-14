ba7836fbe803199e4a27cd654a0333769e9cded5
Daniya Bekzhan <Marozhkakim25@gmail.com>
2020-02-14 14:20:21 +0600

    Добавьте файлы проекта.

---
+++ dz__7/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_7C
+{
+
+    class Chisla
+    {
+        public static void WriteChisla()
+        {
+
+            Console.WriteLine("Введите а");
+            int a = Console.Read();
+            Console.WriteLine("Введите в");
+            int b = Console.Read();
+            int c = a + b;
+            Console.WriteLine("Результат с=" + c);
+
+            int Calc()
+            {
+                int a = 0; int b = 0; int max = 0;
+                if (a > b)
+                {
+                    max = a;
+                }
+                else
+                {
+                    max = b;
+                }
+                return max;
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        {
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
